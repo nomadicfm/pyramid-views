561849637c39146cc6c58c5a8ea5c49db339c7d1
Adam Charnock <adam@omniwiki.co.uk>
2014-03-26 18:16:12 +0000

    Packaging

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ setup(
     name='pyramid-views',
     version=__version__,
     # Your name & email here
-    author='',
-    author_email='',
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
     # If you had pyramid_views.tests, you would also include that in this list
     packages=find_packages(),
     # Any executable scripts, typically in 'bin'. E.g 'bin/do-something.py'
     scripts=[],
     # REQUIRED: Your project's URL
-    url='',
+    url='https://github.com/nomadicfm/pyramid-views',
     # Put your license here. See LICENSE.txt for more information
-    license='',
+    license='MIT',
     # Put a nice one-liner description here
     description='',
     long_description=open('README.rst').read() if exists("README.rst") else "",
