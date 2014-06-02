f6faa9999344f8d21c26becfd6ec1022fe2f2230
Adam Charnock <adam@omniwiki.co.uk>
2014-06-02 18:12:59 +0100

    Version bump to 1.0.8 and updating CHANGES.txt

--- pyramid_views/__init__.py
+++ pyramid_views/__init__.py
@@ -2,7 +2,7 @@
 # It must be possible to import this file with 
 # none of the package's dependencies installed
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 
 session = None
 def configure_views(session_):
